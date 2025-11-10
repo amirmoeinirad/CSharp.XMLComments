e80acb2789dfdbe7d737a000729c8b9785263768
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-11-10 17:18:19 +0330

    Add project files.

---
+++ XML Comments/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XML")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XML_Comments.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c030e1f6-4de9-4faa-89e0-30d29d32c2fc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XML Comments/XML.cs
@@ -0,0 +1,25 @@
+﻿
+// Amir Moeini Rad
+// Nov 2025
+
+// Main Concept: XML documentation
+
+
+/// <summary>
+/// This is a summary describing the class.
+/// </summary>
+/// <remarks>
+/// This is a longer comment that can be used to describe
+/// the class. 
+/// </remarks>
+class XMLApp
+{
+    /// <summary>
+    /// The entry point for the application.
+    /// </summary>
+    /// <param name="args"> A list of command line arguments</param>    
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("An XML Documented Program.");
+    }
+}
